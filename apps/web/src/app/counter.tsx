aa09b8d0b9c05c94a4a3c02b50b63440d22912ba
lizunlong <lizunlong@gmail.com>
2023-05-31 17:25:20 +0800

    fix: format & lint

--- apps/web/src/app/counter.tsx
+++ apps/web/src/app/counter.tsx
@@ -6,17 +6,17 @@ export default function Counter() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full space-y-4">
+    <div className="flex h-full w-full flex-col items-center justify-center space-y-4">
       <h1 className="text-6xl font-bold">{count}</h1>
       <div className="flex space-x-4">
         <button
-          className="px-4 py-2 text-white bg-blue-500 rounded-md"
+          className="rounded-md bg-blue-500 px-4 py-2 text-white"
           onClick={() => setCount(count + 1)}
         >
           Increment
         </button>
         <button
-          className="px-4 py-2 text-white bg-red-500 rounded-md"
+          className="rounded-md bg-red-500 px-4 py-2 text-white"
           onClick={() => setCount(count - 1)}
         >
           Decrement
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+
 import Counter from './counter'
 
 export default function Home() {
