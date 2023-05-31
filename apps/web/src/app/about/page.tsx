28f2f53f820fbcee5cd71db85f680c4810610f4b
lizunlong <lizunlong@gmail.com>
2023-05-31 21:38:01 +0800

    fix: it works

--- apps/web/src/app/about/page.tsx
+++ apps/web/src/app/about/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function AboutPage() {
   return (
     <div className="p-2">
-      <h1>About ? Page</h1>
+      <h1>About Page</h1>
       <Link
         href="/dashboard"
         className="text-blue-600 hover:underline dark:text-blue-500"
