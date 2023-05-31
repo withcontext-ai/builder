983d27950df29ba7091e3f88db4c2e3deda37ae0
dailly <daillyf@163.com>
2023-05-31 19:44:46 +0800

    test: tailcss-test

--- apps/web/src/app/test/page.tsx
+++ apps/web/src/app/test/page.tsx
@@ -1,8 +1,9 @@
 const Test = ()=>{
   console.log('----------test')
-  return <div className="bg-white flex align-item-center w-10 max-h-10 p-24 flex-direction-row">
-    <h6 className="text-md divide-y divide-gray-200 w-8 display-inline">this is test page</h6>
-  </div>
+  return <div className="bg-white flex items-center w-full h-full p-24 flex-direction-row">
+    <p className="text-slate-500">this is test page</p>
+    <div className="chat-notification"></div>
+    </div>
 }
 
 export default Test
