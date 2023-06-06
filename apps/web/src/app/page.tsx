ef568ab610d5c94ca17b1786832c60528af82d29
lizunlong <lizunlong@gmail.com>
2023-06-06 19:47:42 +0800

    style: improve layout

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -39,17 +39,24 @@ export default async function Home() {
   const email = emailAddresses?.[0].emailAddress
 
   return (
-    <div>
-      <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-50 lg:block lg:w-18 lg:overflow-y-auto lg:bg-gray-900 lg:pb-4 lg:pt-6">
-        <div className="flex shrink-0 items-center justify-center">
+    <div className="flex max-h-full overflow-hidden">
+      <div className="flex w-18 flex-col bg-slate-900">
+        <div className="mt-6 flex shrink-0 items-center justify-center">
           <Avatar className="h-12 w-12 bg-white">
             <AvatarImage src="https://github.com/withcontext-ai.png" />
             <AvatarFallback>CO</AvatarFallback>
           </Avatar>
         </div>
-        <div className="m-auto my-6 h-px w-14 bg-slate-200" />
-        <nav className="mt-8">
+        <div className="m-auto mt-6 h-px w-14 bg-slate-200" />
+        <nav className="overflow-y-auto py-6">
           <ul role="list" className="flex flex-col items-center space-y-4">
+            <li>
+              <Avatar className="h-12 w-12">
+                <AvatarFallback className=" bg-white">
+                  <Plus />
+                </AvatarFallback>
+              </Avatar>
+            </li>
             <li>
               <Avatar className="h-12 w-12 bg-white">
                 <AvatarImage src="https://github.com/lzl.png" />
@@ -62,34 +69,22 @@ export default async function Home() {
                 <AvatarFallback>CN</AvatarFallback>
               </Avatar>
             </li>
-            <li>
-              <Avatar className="h-12 w-12">
-                <AvatarFallback className=" bg-white">
-                  <Plus />
-                </AvatarFallback>
-              </Avatar>
-            </li>
+            {[...Array(10)].map((_, i) => (
+              <li key={i}>
+                <Avatar className="h-12 w-12 bg-white">
+                  <AvatarFallback>B{i}</AvatarFallback>
+                </Avatar>
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
 
-      <main className="lg:pl-18">
-        <div className="xl:pl-60">
-          <div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">
-            <div className="h-96">Main area</div>
-            <div className="h-96">Main area</div>
-            <div className="h-96">Main area</div>
-            <div className="h-96">Main area</div>
-          </div>
-        </div>
-      </main>
-
-      <aside className="fixed inset-y-0 left-18 hidden w-60 flex-col overflow-y-auto border-r border-gray-200 xl:flex">
-        <div className="flex-1 divide-y divide-slate-100">
-          <h1 className="px-6 py-4 text-2xl font-semibold">Explore</h1>
-          <div className="px-1 py-3">
-            <SidebarNav items={sidebarNavItems} />
-          </div>
+      <aside className="flex w-60 flex-col border-r border-gray-200">
+        <h1 className="px-6 py-4 text-2xl font-semibold">Explore</h1>
+        <div className="m-full h-px bg-slate-100" />
+        <div className="flex-1 overflow-y-auto px-1 py-3">
+          <SidebarNav items={sidebarNavItems} />
         </div>
         <div className="flex h-16 items-center bg-slate-100 px-4">
           {userId ? (
@@ -108,6 +103,13 @@ export default async function Home() {
           )}
         </div>
       </aside>
+
+      <main className="flex-1 overflow-y-auto px-8 py-6">
+        <div className="h-96">Main area</div>
+        <div className="h-96">Main area</div>
+        <div className="h-96">Main area</div>
+        <div className="h-96">Main area</div>
+      </main>
     </div>
   )
 }
