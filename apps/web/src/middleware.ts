ebb7fefde9e8d98d80b0acf6c0d462c61e00687a
lizunlong <lizunlong@gmail.com>
2023-06-12 12:26:43 +0800

    feat: enabledAuth

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,7 +1,17 @@
+import { NextResponse } from 'next/server'
 import { authMiddleware } from '@clerk/nextjs'
 
+import { getFlags } from './lib/flags'
+
+const { isDev, enabledAuth } = getFlags()
+
 export default authMiddleware({
+  debug: isDev,
   publicRoutes: ['/', '/explore'],
+  beforeAuth: () => {
+    if (enabledAuth) return NextResponse.next()
+    return false
+  },
 })
 
 export const config = {
