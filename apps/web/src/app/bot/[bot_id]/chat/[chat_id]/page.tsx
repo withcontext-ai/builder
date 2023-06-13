e5e9ae1bf06b2788aff7024f01b6be3fb4b2270a
daillfy <44747176+daillfy@users.noreply.github.com>
2023-06-13 19:36:50 +0800

    feat: upload component (#7)
    * feat: upload compnent
    
    * feat: upload compnent
    
    * chore: just a test
    
    * fix: upload eslint
    
    * feat: add chatpage
    
    * feat: add markdown
    
    * chore: fetch merge main
    
    * chore: pnpm-lock.yaml
    
    * fix: eslint
    
    * fix: upload button
    
    * fix: build problem
    
    * fix: pnpm lock file
    
    * chore: put TestUpload on demo page
    
    * fix: format
    
    * fix: upload eslint
    
    * fix: remove unuseable upload params
    
    * fix: add upload demo type
    
    * fix: remove ts-ignore
    
    * fix: recover expore page
    
    * feat: run lint:fix
    
    * fix: run eslint
    
    ---------
    
    Co-authored-by: zdx <zdx@zdxMc.local>
    Co-authored-by: lizunlong <lizunlong@gmail.com>

--- apps/web/src/app/bot/[bot_id]/chat/[chat_id]/page.tsx
+++ apps/web/src/app/bot/[bot_id]/chat/[chat_id]/page.tsx
@@ -4,7 +4,6 @@ interface IProps {
 
 export default function ChatPage({ params }: IProps) {
   const { chat_id } = params
-
   // TODO: put <Chat id={chat_id} /> component into this page
   // and use chat_id to fetch chat data
   return <div>Chat: {chat_id}</div>
--- apps/web/src/components/app-layout.tsx
+++ apps/web/src/components/app-layout.tsx
@@ -7,7 +7,7 @@ export default function AppLayout({ sidebar, children }: IProps) {
   return (
     <div className="flex max-h-full overflow-hidden">
       {sidebar ? (
-        <div className="w-18 flex flex-col bg-slate-900">{sidebar}</div>
+        <div className="flex w-18 flex-col bg-slate-900">{sidebar}</div>
       ) : null}
       {children}
     </div>
