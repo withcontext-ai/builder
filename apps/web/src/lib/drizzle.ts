b672e9ebe70f510a286658673dcc70b9b890fb73
lizunlong <lizunlong@gmail.com>
2023-06-15 01:20:35 +0800

    fix: so tricky

--- apps/web/src/lib/drizzle.ts
+++ apps/web/src/lib/drizzle.ts
@@ -37,7 +37,7 @@ const pool = flags.isNeon
   : new Pool({
       connectionString:
         process.env.DATABASE_URL ||
-        'postgres://postgres:postgres@host.docker.internal:5432/postgres',
+        'postgres://postgres:postgres@db:5432/postgres',
     })
 
 export const db = flags.isNeon ? drizzleServerless(pool) : drizzle(pool)
