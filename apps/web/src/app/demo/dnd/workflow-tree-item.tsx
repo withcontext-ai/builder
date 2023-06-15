bf86fbe246dc45c356ce427846283d702b0592a4
lizunlong <lizunlong@gmail.com>
2023-06-15 13:05:59 +0800

    style: workflow

--- apps/web/src/app/demo/dnd/workflow-tree-item.tsx
+++ apps/web/src/app/demo/dnd/workflow-tree-item.tsx
@@ -11,7 +11,11 @@ export default function WorkflowTreeItem({
   }
 
   return (
-    <div className="relative w-80" {...handleProps} onClick={handleClick}>
+    <div
+      className="relative mb-4 w-[360px] rounded-lg border bg-white p-4"
+      {...handleProps}
+      onClick={handleClick}
+    >
       id: {id}
       {clone && childCount && childCount > 1 ? (
         <span className="absolute right-[-10px] top-[-10px] flex h-6 w-6 items-center justify-center rounded-full bg-blue-600 text-xs font-semibold text-white">
