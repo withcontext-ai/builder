8a3e7b0e41c58be2353d57a43e55233f4745384c
lizunlong <lizunlong@gmail.com>
2023-06-15 14:23:55 +0800

    fix: config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,9 @@
 module.exports = {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    'pages/**/*.{js,ts,jsx,tsx,mdx}',
+    'components/**/*.{js,ts,jsx,tsx,mdx}',
+    'app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
