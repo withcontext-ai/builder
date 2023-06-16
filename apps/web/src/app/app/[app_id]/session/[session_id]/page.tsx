62e4271fef62fbf977453194ed0ae26aceb7a2e5
lizunlong <lizunlong@gmail.com>
2023-06-16 16:45:48 +0800

    fix: style

--- apps/web/src/app/app/[app_id]/session/[session_id]/page.tsx
+++ apps/web/src/app/app/[app_id]/session/[session_id]/page.tsx
@@ -1,3 +1,5 @@
+import Chat from '@/components/chat/page'
+
 interface IProps {
   params: { session_id: string }
 }
@@ -6,5 +8,9 @@ export default function SessionPage({ params }: IProps) {
   const { session_id } = params
   // TODO: put <Chat id={session_id} /> component into this page
   // and use session_id to fetch session data
-  return <div>Session: {session_id}</div>
+  return (
+    <div className="h-full w-full overflow-hidden">
+      <Chat />
+    </div>
+  )
 }
--- apps/web/src/components/sidebar-layout.tsx
+++ apps/web/src/components/sidebar-layout.tsx
@@ -1,24 +1,16 @@
-import { cn } from '@/lib/utils'
-
 interface IProps {
   sidebar: React.ReactNode
   children: React.ReactNode
   mainClassName?: string
 }
 
-export default function SidebarLayout({
-  sidebar,
-  children,
-  mainClassName,
-}: IProps) {
+export default function SidebarLayout({ sidebar, children }: IProps) {
   return (
     <div className="flex flex-1">
       <aside className="flex w-60 flex-col border-r border-slate-100">
         {sidebar}
       </aside>
-      <main className={cn('flex-1 overflow-y-auto px-8 py-6 ', mainClassName)}>
-        {children}
-      </main>
+      <main className={'flex-1 overflow-y-auto'}>{children}</main>
     </div>
   )
 }
