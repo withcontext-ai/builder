4cdfede1786c9f31b7bd3436d097b8f496a8f665
lizunlong <lizunlong@gmail.com>
2023-06-27 17:58:19 +0800

    feat: use icon

--- apps/web/src/app/app/[app_id]/sidebar-header.tsx
+++ apps/web/src/app/app/[app_id]/sidebar-header.tsx
@@ -11,10 +11,13 @@ export default function Header({ appId, name, desc, icon }: IProps) {
   return (
     <div>
       <div
-        className="h-32 bg-black bg-cover bg-center"
+        className="h-32 bg-cover bg-center"
         style={{
-          backgroundImage:
-            "url('https://backend.withcontext.ai/backend/upload/2023/04/65947928-68d6-4f64-99d9-0b98578fe4c6.jpeg')",
+          ...(icon
+            ? {
+                backgroundImage: `url(${icon})`,
+              }
+            : {}),
         }}
       >
         <div className="flex items-center justify-between bg-gradient-to-b from-slate-500 to-transparent px-4 py-2.5 text-white">
