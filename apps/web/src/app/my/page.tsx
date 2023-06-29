1d11c259ad82a3aa427db2067a9cff8c666685d3
daillfy <44747176+daillfy@users.noreply.github.com>
2023-06-29 17:08:24 +0800

    feat: create my app

--- apps/web/src/app/my/page.tsx
+++ apps/web/src/app/my/page.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
+import CreateAppDialog from '@/components/create-app-dialog'
 import CategoriesSidebar from '@/components/sidebar-categories'
 import SidebarLayout from '@/components/sidebar-layout'
 
@@ -10,7 +13,8 @@ const MyApps = () => {
       <div className="flex flex-col">
         <div className="flex flex-col">
           <div className="flex items-center justify-between px-6 py-1 text-base font-medium leading-[48px]">
-            My Apps <Button>Create App</Button>
+            My Apps
+            <CreateAppDialog dialogTrigger={<Button>Create App</Button>} />
           </div>
           <div className="h-[1px] w-full bg-slate-100 " />
         </div>
