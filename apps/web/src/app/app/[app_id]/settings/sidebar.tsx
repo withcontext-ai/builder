8949a575f4a1d00bce59d03d524e862090f199af
lizunlong <lizunlong@gmail.com>
2023-06-30 21:14:09 +0800

    fix: go back button

--- apps/web/src/app/app/[app_id]/settings/sidebar.tsx
+++ apps/web/src/app/app/[app_id]/settings/sidebar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Link from 'next/link'
-import { useParams, usePathname } from 'next/navigation'
+import { useParams, usePathname, useRouter } from 'next/navigation'
 import { ArrowLeftIcon } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
@@ -11,12 +11,22 @@ const commonStyle =
   'flex cursor-pointer flex-col rounded-md p-3 hover:bg-slate-100'
 
 export default function Sidebar() {
+  const router = useRouter()
   const { app_id: appId } = useParams()
   const url = usePathname() || ''
+
+  function handleGoBack() {
+    router.back()
+  }
+
   return (
     <div>
       <div className="flex items-center space-x-2 px-4 py-3">
-        <Button variant="outline" className="h-8 w-8 p-0">
+        <Button
+          variant="outline"
+          className="h-8 w-8 p-0"
+          onClick={handleGoBack}
+        >
           <ArrowLeftIcon className="h-4 w-4" />
         </Button>
         <div className="text-lg font-semibold">Back</div>
@@ -38,7 +48,7 @@ export default function Sidebar() {
             Some basic configurations of the App.
           </div>
         </Link>
-        <Link
+        {/* <Link
           className={cn(
             commonStyle,
             url?.includes('workflow') ? 'bg-slate-100' : ''
@@ -62,7 +72,7 @@ export default function Sidebar() {
           <div className="text-sm text-slate-500">
             Import your own data for LLM context enhancement.
           </div>
-        </Link>
+        </Link> */}
       </div>
     </div>
   )
