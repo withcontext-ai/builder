649294d94727f9537bb48f277d11b4219cffcd56
lizunlong <lizunlong@gmail.com>
2023-07-02 22:07:11 +0800

    feat: share app logic

---
+++ apps/web/src/app/app/[app_id]/session/[session_id]/add-app-to-workspace.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import * as React from 'react'
+import { useSWRConfig } from 'swr'
+import useSWRMutation from 'swr/mutation'
+
+import { fetcher } from '@/lib/utils'
+
+function addToWorkspace(url: string, { arg }: { arg: { app_id: string } }) {
+  return fetcher(url, {
+    method: 'POST',
+    body: JSON.stringify(arg),
+  })
+}
+
+interface IProps {
+  appId: string
+}
+
+export default function AddAppToWorkspace({ appId }: IProps) {
+  // const { mutate } = useSWRConfig()
+  const { trigger } = useSWRMutation(`/api/me/workspace`, addToWorkspace)
+
+  React.useEffect(() => {
+    // async function init() {
+    //   const result = await trigger({ app_id: appId })
+    // }
+
+    // init()
+    trigger({ app_id: appId })
+  }, [appId, trigger])
+
+  return null
+}
