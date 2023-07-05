3c69d57c01970adfc617e5d5f0965b8fc5311fbf
lizunlong <lizunlong@gmail.com>
2023-07-05 18:15:27 +0800

    style: add task detail on settings page

--- apps/web/src/app/app/[app_id]/settings/workflow/page.tsx
+++ apps/web/src/app/app/[app_id]/settings/workflow/page.tsx
@@ -1,15 +1,21 @@
 import AddTaskButton from './add-task-button'
 import TaskList from './task-list'
 
-export default function WorkflowSettingsPage() {
+export default function Page() {
   return (
-    <div className="h-full overflow-auto px-14 pt-16">
-      <h1 className="text-2xl font-semibold">Workflow</h1>
-      <div className="mt-6">
-        <TaskList />
+    <div className="flex h-full">
+      <div className="flex-1 overflow-auto px-14 pt-16">
+        <h1 className="text-2xl font-semibold">Workflow</h1>
+        <div className="mt-6">
+          <TaskList />
+        </div>
+        <div className="mt-4">
+          <AddTaskButton />
+        </div>
       </div>
-      <div className="mt-4">
-        <AddTaskButton />
+
+      <div className="w-[380px] shrink-0 overflow-auto border-l border-slate-200 p-6">
+        task detail
       </div>
     </div>
   )
