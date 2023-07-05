b7ecb3febb2a819a065050063fb5fa614a80141d
lizunlong <lizunlong@gmail.com>
2023-07-05 10:12:58 +0800

    feat: dataset card

--- apps/web/src/app/apps/page.tsx
+++ apps/web/src/app/apps/page.tsx
@@ -1,9 +1,50 @@
 import { PlusIcon } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
+import AppCard from '@/components/app-card'
 import CreateAppDialog from '@/components/create-app-dialog'
 import HomeSidebar from '@/components/home-sidebar'
 import SidebarLayout from '@/components/sidebar-layout'
+
+const LIST = [
+  {
+    id: 'CbfWtV8',
+    title: 'App 1, App 1, App 1, App 1, App 1',
+    description:
+      'App 1 Description, App 1 Description, App 1 Description, App 1 Description, App 1 Description, App 1 Description, App 1 Description, App 1 Description, App 1 Description, App 1 Description, App 1 Description, App 1 Description,',
+    icon: '',
+  },
+  {
+    id: 'Utxx7ga',
+    title: 'App 2',
+    description: 'App 2 Description',
+    icon: '',
+  },
+  {
+    id: 'GI6hAFE',
+    title: 'App 3',
+    description: 'App 3 Description',
+    icon: '',
+  },
+  {
+    id: 'WMZeCI3',
+    title: 'App 4',
+    description: 'App 4 Description',
+    icon: '',
+  },
+  {
+    id: 'gRBqVWJ',
+    title: 'App 5',
+    description: 'App 5 Description',
+    icon: '',
+  },
+  {
+    id: 'Zq2go3j',
+    title: 'App 6',
+    description: 'App 6 Description',
+    icon: '',
+  },
+]
 
 export default async function Page() {
   return (
@@ -21,6 +62,19 @@ export default async function Page() {
           />
         </div>
         <div className="m-full h-px bg-slate-100" />
+        <div className="p-6">
+          <ul className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {LIST.map(({ id, title, description, icon }) => (
+              <AppCard
+                key={id}
+                id={id}
+                title={title}
+                description={description}
+                icon={icon}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
     </SidebarLayout>
   )
