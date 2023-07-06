a07a0b61ae18365e576373776bd71f07f4ac2953
lizunlong <lizunlong@gmail.com>
2023-07-06 17:13:51 +0800

    chore: hide categories

--- apps/web/src/components/explore-list.tsx
+++ apps/web/src/components/explore-list.tsx
@@ -5,22 +5,22 @@ const DEFAULT_VALUE = [
     href: '/explore',
     title: 'All Categories',
   },
-  {
-    href: '/explore/hr',
-    title: 'Human Resources',
-  },
-  {
-    href: '/explore/tr',
-    title: 'Translation',
-  },
-  {
-    href: '/explore/kb',
-    title: 'Knowledge Base',
-  },
-  {
-    href: '/explore/st',
-    title: 'Self Training',
-  },
+  // {
+  //   href: '/explore/hr',
+  //   title: 'Human Resources',
+  // },
+  // {
+  //   href: '/explore/tr',
+  //   title: 'Translation',
+  // },
+  // {
+  //   href: '/explore/kb',
+  //   title: 'Knowledge Base',
+  // },
+  // {
+  //   href: '/explore/st',
+  //   title: 'Self Training',
+  // },
 ]
 
 export default function ExploreList() {
