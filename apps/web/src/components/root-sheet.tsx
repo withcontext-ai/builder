d7e2ab058e1c2bbf79e051058cf1524070bb6752
lizunlong <lizunlong@gmail.com>
2023-07-14 15:26:51 +0800

    feat: apps page

--- apps/web/src/components/root-sheet.tsx
+++ apps/web/src/components/root-sheet.tsx
@@ -29,30 +29,33 @@ export default function RootSheet({ children, defaultPageTitle }: IProps) {
   }, [path, setIsOpen])
 
   return (
-    <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-white p-4 shadow-sm sm:px-6 lg:hidden">
-      <Sheet open={isOpen} onOpenChange={setIsOpen}>
-        <SheetTrigger className="-m-2.5 p-2.5 text-gray-700 lg:hidden">
-          <span className="sr-only">Open sidebar</span>
-          <MenuIcon className="h-6 w-6" aria-hidden="true" />
-        </SheetTrigger>
-        <SheetContent side="left" className="w-auto p-0">
-          {children}
-          <SheetClose
-            className={cn(
-              'absolute -right-8 top-4 rounded-sm opacity-0 ring-offset-background transition-opacity data-[state=open]:bg-secondary hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none',
-              isOpen && 'opacity-70'
-            )}
-          >
-            <XIcon className="h-4 w-4" />
-            <span className="sr-only">Close sidebar</span>
-          </SheetClose>
-        </SheetContent>
-      </Sheet>
-      {pageTitle && (
-        <div className="flex-1 text-sm font-semibold leading-6 text-gray-900">
-          {pageTitle}
-        </div>
-      )}
-    </div>
+    <>
+      <div className="fixed inset-x-0 top-0 z-40 flex h-12 items-center gap-x-6 bg-white px-4 shadow-sm sm:px-6 lg:hidden">
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger className="-m-2.5 p-2.5 text-gray-700 lg:hidden">
+            <span className="sr-only">Open sidebar</span>
+            <MenuIcon className="h-6 w-6" aria-hidden="true" />
+          </SheetTrigger>
+          <SheetContent side="left" className="w-auto p-0">
+            {children}
+            <SheetClose
+              className={cn(
+                'absolute -right-8 top-4 rounded-sm opacity-0 ring-offset-background transition-opacity data-[state=open]:bg-secondary hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none',
+                isOpen && 'opacity-70'
+              )}
+            >
+              <XIcon className="h-4 w-4" />
+              <span className="sr-only">Close sidebar</span>
+            </SheetClose>
+          </SheetContent>
+        </Sheet>
+        {pageTitle && (
+          <div className="flex-1 text-sm font-semibold leading-6 text-gray-900">
+            {pageTitle}
+          </div>
+        )}
+      </div>
+      <div className="h-12 lg:hidden" />
+    </>
   )
 }
