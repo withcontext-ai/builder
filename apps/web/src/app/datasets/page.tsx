116df2e9a96a247956ac1723c08a014b81220597
lizunlong <lizunlong@gmail.com>
2023-07-14 16:05:33 +0800

    feat: responsive design for apps page (#58)
    * feat: apps page
    
    * feat: dataset app

--- apps/web/src/app/datasets/page.tsx
+++ apps/web/src/app/datasets/page.tsx
@@ -11,13 +11,19 @@ export default async function Page() {
   return (
     <RootWrapper pageTitle="My Datasets">
       <div className="flex flex-col">
+        {/* desktop version */}
         <div className="hidden h-12 items-center justify-between px-6 lg:flex">
           <h1 className="font-medium">My Datasets</h1>
           <CreateDialog />
         </div>
-        <div className="m-full h-px bg-slate-100" />
+        {/* mobile version */}
+        {/* <div className="fixed left-10 right-0 top-0 z-40 flex h-12 items-center justify-between bg-white px-6 lg:hidden">
+          <h1 className="font-medium">My Datasets</h1>
+          <CreateDialog />
+        </div> */}
+        <div className="m-full hidden h-px shrink-0 bg-slate-100 lg:block" />
         <div className="p-6">
-          <ul className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {datasets?.map(({ short_id, name }) => (
               <DatasetCard key={short_id} id={short_id} title={name} />
             ))}
