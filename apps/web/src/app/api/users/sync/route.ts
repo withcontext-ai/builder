01bceea277b6db73e0dfb5de8a4ded290705b45f
lizunlong <lizunlong@gmail.com>
2023-07-19 18:09:46 +0800

    feat: sync users

---
+++ apps/web/src/app/api/users/sync/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from 'next/server'
+import { clerkClient } from '@clerk/nextjs'
+
+import { auth } from '@/lib/auth'
+import { addUser } from '@/db/users/actions'
+
+export async function GET() {
+  const { userId } = auth()
+  if (!userId) {
+    return NextResponse.redirect('/')
+  }
+
+  const totalUsers = await clerkClient.users.getCount()
+
+  const users = await clerkClient.users.getUserList({
+    limit: 100,
+  })
+
+  users.forEach((user) => {
+    const userValue = {
+      short_id: user.id,
+      last_name: user.lastName,
+      first_name: user.firstName,
+      image_url: user.imageUrl,
+      username: user.username,
+      created_at: new Date(user.createdAt),
+    }
+    addUser(userValue)
+  })
+
+  return NextResponse.json({
+    success: true,
+    data: {
+      totalUsers,
+      users,
+    },
+  })
+}
