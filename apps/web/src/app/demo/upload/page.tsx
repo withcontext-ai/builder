7d54c53429b86a67f4fbef13623361bfb6138d05
daillfy <44747176+daillfy@users.noreply.github.com>
2023-07-19 20:35:51 +0800

    refactor: upload component (#49)
    * feat: upload compnent
    
    * feat: upload compnent
    
    * chore: just a test
    
    * fix: upload eslint
    
    * feat: add chatpage
    
    * feat: add markdown
    
    * chore: fetch merge main
    
    * chore: pnpm-lock.yaml
    
    * fix: eslint
    
    * fix: upload button
    
    * fix: build problem
    
    * fix: pnpm lock file
    
    * chore: put TestUpload on demo page
    
    * fix: format
    
    * fix: upload eslint
    
    * fix: remove unuseable upload params
    
    * fix: add upload demo type
    
    * fix: remove ts-ignore
    
    * fix: recover expore page
    
    * feat: run lint:fix
    
    * fix: run eslint
    
    * feat: upload ability demo
    
    * fix: beforeUpload valid
    
    * fix: beforeUpload deom
    
    * fix: showListCard
    
    * feat: drag upload demo
    
    * fix: cancel upload
    
    * fix: button type
    
    * feat: add confirm when broswer close
    
    * fix: unload callback
    
    * fix: check file uploading
    
    * fix: image url to uploadFile
    
    * fix: show default pdf &image
    
    * fix: handleFiles & showUploadList
    
    * fix: show default icons
    
    * fix: show default files & uid
    
    * fix: cr
    
    * fix: eslint
    
    * fix: pnpm build eslint
    
    * fix: onChangeFileList
    
    * fix: change uploadFiles to fileProps
    
    * fix: upload remove
    
    * fix: show uploading file
    
    * fix: remove unuse code
    
    * feat: add update-image listType
    
    * fix: upload demo
    
    * fix: dataset upload
    
    * fix: eslint
    
    * fix: check basic form update
    
    * fix: ting change
    
    * fix: update image
    
    * fix: remove uploading confirm
    
    * refactor: app basic setting
    
    ---------
    
    Co-authored-by: zdx <zdx@zdxMc.local>
    Co-authored-by: lizunlong <lizunlong@gmail.com>

---
+++ apps/web/src/app/demo/upload/page.tsx
@@ -0,0 +1,102 @@
+'use client'
+
+import { useState } from 'react'
+
+import { RcFile } from '@/components/upload/type'
+import Upload from '@/components/upload/upload'
+import { FileProps } from '@/components/upload/utils'
+
+const UploadScenes = () => {
+  const defaultPdf = {
+    url: 'https://storage.googleapis.com/context-builder/public-tmp/tbkDzOBF4Fat.pdf',
+    name: 'test.pdf',
+  }
+  const defaultUrl = {
+    url: ' https://backend.withcontext.ai/backend/upload/2023/04/65947928-68d6-4f64-99d9-0b98578fe4c6.jpeg',
+    name: '1.jpeg',
+  }
+  const [files, setFiles] = useState<FileProps[]>([defaultPdf])
+  const [images, setImages] = useState<FileProps[]>([defaultUrl])
+  const [image, setImage] = useState<FileProps[]>([])
+
+  const [current, setCurrent] = useState<FileProps[]>([])
+  const [dragFile, setDragFile] = useState<FileProps[]>([])
+
+  const beforeUpload = (info: RcFile) => {
+    if (info?.size > 1024 * 1024 * 5) {
+      console.log('the file is larger than 5M')
+      return false
+    }
+    return true
+  }
+  return (
+    <div className="w-[960px] space-y-8 p-6">
+      <section className="flex w-[600px] flex-col justify-start space-y-3">
+        <h1 className="text-xl	">pdf list</h1>
+        <Upload
+          fileList={files}
+          accept=".pdf"
+          beforeUpload={beforeUpload}
+          // default show all icons(delete, preview & download),if listProps=false show no icon
+          /***
+           * default api is google cloud , just use onChangeFileList to get the uploadFiles you need
+           * you can also to by onChange to use your own api
+           *  */
+          onChangeFileList={(files) => {
+            setFiles([...files])
+          }}
+        />
+      </section>
+
+      <section className="flex w-[600px] flex-col justify-start space-y-3">
+        <h1 className="text-xl	"> show default image list</h1>
+        <Upload
+          listType="images-list"
+          accept=".png,jpeg,.jpg,.webp"
+          fileList={images}
+          onChangeFileList={(files) => setImages([...files])}
+        />
+      </section>
+
+      <section className="flex w-[600px] flex-col justify-start space-y-3">
+        <h1 className="text-xl">
+          image: one step: delete the image ,tow step:upload a new
+        </h1>
+        <Upload
+          listType="image"
+          fileList={image}
+          accept=".png,jpeg,.jpg,.webp"
+          onChangeFileList={(files) => setImage(files)}
+        />
+      </section>
+
+      <section className="flex w-[600px] flex-col justify-start space-y-3">
+        <h1 className="text-xl">
+          update image: just one step upload a new and to update the current
+        </h1>
+
+        <Upload
+          listType="update-image"
+          accept=".png,jpeg,.jpg,.webp"
+          showFileList={false}
+          fileList={current}
+          onChangeFileList={(files) => {
+            setCurrent(files)
+          }}
+        />
+      </section>
+
+      <section className="flex w-[600px] flex-col justify-start space-y-3">
+        <h1 className="text-xl">drag multiple files</h1>
+        <Upload
+          fileList={dragFile}
+          type="drag"
+          onChangeFileList={(files) => setDragFile([...files])}
+          // onRemove={handleRemove}
+        />
+      </section>
+    </div>
+  )
+}
+
+export default UploadScenes
--- apps/web/src/components/upload/test.tsx
+++ apps/web/src/components/upload/test.tsx
@@ -2,23 +2,18 @@
 
 import { useState } from 'react'
 
-import { UploadFile, UploadProps } from './type'
+import { UploadProps } from './type'
 import Upload from './upload'
+import { FileProps } from './utils'
 
 const TestUpload = (props: UploadProps) => {
-  const [files, setFiles] = useState<UploadFile[]>([])
+  const [files, setFiles] = useState<FileProps[]>([])
 
-  const handleRemove = (file: UploadFile) => {
-    const data = files?.filter((item) => item?.uid !== file?.uid)
-    setFiles(data)
-  }
   return (
     <Upload
       fileList={files}
-      handleFiles={(files) => setFiles([...files])}
-      onRemove={handleRemove}
+      onChangeFileList={(files) => setFiles([...files])}
       // 自定义请求方式，这里在onChange阶段调用的Google cloud，不处理会使用rc-upload的请求方式多发送一次请求
-      customRequest={() => {}}
       {...props}
     />
   )
