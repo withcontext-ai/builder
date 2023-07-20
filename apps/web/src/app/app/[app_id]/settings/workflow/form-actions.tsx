d0af571bc495aaa634c5becb57a52774b8bb1dfc
lizunlong <lizunlong@gmail.com>
2023-07-20 15:34:14 +0800

    style: foot bar layout

--- apps/web/src/app/app/[app_id]/settings/workflow/form-actions.tsx
+++ apps/web/src/app/app/[app_id]/settings/workflow/form-actions.tsx
@@ -8,8 +8,8 @@ export default function FormActions() {
   const { reset } = useFormContext()
 
   return (
-    <div className="fixed bottom-4 left-1/2 -translate-x-1/2">
-      <div className="flex h-18 w-[600px] items-center justify-end space-x-2 rounded-lg border border-slate-100 bg-background px-4 shadow-md">
+    <div className="fixed bottom-4 left-[276px] mx-4">
+      <div className="flex h-18 w-[600px] max-w-md items-center justify-end space-x-2 rounded-lg border border-slate-100 bg-background px-4 shadow-md lg:max-w-lg xl:max-w-xl 2xl:max-w-full">
         <Button type="button" variant="ghost" onClick={() => reset()}>
           Reset
         </Button>
