61b8b3989b1c4e9d7183b774169ebc2f7231c2a1
lizunlong <lizunlong@gmail.com>
2023-07-20 15:51:43 +0800

    fix: add task button

--- apps/web/src/app/app/[app_id]/settings/workflow/add-task-button.tsx
+++ apps/web/src/app/app/[app_id]/settings/workflow/add-task-button.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react'
 import { useSettingsStore, WorkflowType } from '@/store/settings'
-import { LightbulbIcon, PlusIcon } from 'lucide-react'
+import { PlusIcon, WrenchIcon } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 import {
@@ -28,11 +28,15 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip'
 
+const ICON_MAP = {
+  chains: WrenchIcon,
+}
+
 const CONFIG = [
   {
     type: 'chains',
     title: 'Tools',
-    desc: 'bla bla bla',
+    desc: 'Some encapsulated tools to handle certain tasks.',
     children: [
       {
         title: 'Tools',
@@ -132,7 +136,19 @@ const CONFIG = [
   //     },
   //   ],
   // },
-]
+] satisfies {
+  type: 'chains' | 'agents'
+  title: string
+  desc: string
+  children: {
+    title: string
+    children: {
+      subType: string
+      title: string
+      desc: string
+    }[]
+  }[]
+}[]
 
 export default function AddTaskButton() {
   const [open, setOpen] = React.useState(false)
@@ -152,53 +168,59 @@ export default function AddTaskButton() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-96" align="start">
-        {CONFIG.map((item) => (
-          <DropdownMenuSub key={item.type}>
-            <DropdownMenuSubTrigger>
-              <LightbulbIcon className="mr-3 mt-1 h-6 w-6 shrink-0 self-start" />
-              <div className="flex-1">
-                <div className="font-medium">{item.title}</div>
-                <div className="mt-1 text-slate-500">{item.desc}</div>
-              </div>
-            </DropdownMenuSubTrigger>
-            <DropdownMenuPortal>
-              <DropdownMenuSubContent className="w-80 p-0">
-                <Command>
-                  <CommandInput placeholder="Search" />
-                  <CommandList>
-                    <CommandEmpty>No results found.</CommandEmpty>
-                    {item.children.map((group) => (
-                      <CommandGroup key={group.title} heading={group.title}>
-                        {group.children.map((subItem) => (
-                          <CommandItem
-                            key={subItem.subType}
-                            onSelect={selectHandler(item.type, subItem.subType)}
-                          >
-                            <Tooltip>
-                              <TooltipTrigger asChild>
-                                <div className="flex-1 truncate">
-                                  <div className="font-medium">
-                                    {subItem.title}
+        {CONFIG.map((item) => {
+          const Icon = ICON_MAP[item.type]
+          return (
+            <DropdownMenuSub key={item.type}>
+              <DropdownMenuSubTrigger>
+                <Icon className="mr-3 mt-1 h-6 w-6 shrink-0 self-start" />
+                <div className="flex-1">
+                  <div className="font-medium">{item.title}</div>
+                  <div className="mt-1 text-slate-500">{item.desc}</div>
+                </div>
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent className="w-80 p-0">
+                  <Command>
+                    <CommandInput placeholder="Search" />
+                    <CommandList>
+                      <CommandEmpty>No results found.</CommandEmpty>
+                      {item.children.map((group) => (
+                        <CommandGroup key={group.title} heading={group.title}>
+                          {group.children.map((subItem) => (
+                            <CommandItem
+                              key={subItem.subType}
+                              onSelect={selectHandler(
+                                item.type,
+                                subItem.subType
+                              )}
+                            >
+                              <Tooltip>
+                                <TooltipTrigger asChild>
+                                  <div className="flex-1 truncate">
+                                    <div className="font-medium">
+                                      {subItem.title}
+                                    </div>
+                                    <div className="mt-1 truncate text-slate-500">
+                                      {subItem.desc}
+                                    </div>
                                   </div>
-                                  <div className="mt-1 truncate text-slate-500">
-                                    {subItem.desc}
-                                  </div>
-                                </div>
-                              </TooltipTrigger>
-                              <TooltipContent side="right">
-                                <p className="max-w-xs">{subItem.desc}</p>
-                              </TooltipContent>
-                            </Tooltip>
-                          </CommandItem>
-                        ))}
-                      </CommandGroup>
-                    ))}
-                  </CommandList>
-                </Command>
-              </DropdownMenuSubContent>
-            </DropdownMenuPortal>
-          </DropdownMenuSub>
-        ))}
+                                </TooltipTrigger>
+                                <TooltipContent side="right">
+                                  <p className="max-w-xs">{subItem.desc}</p>
+                                </TooltipContent>
+                              </Tooltip>
+                            </CommandItem>
+                          ))}
+                        </CommandGroup>
+                      ))}
+                    </CommandList>
+                  </Command>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+          )
+        })}
       </DropdownMenuContent>
     </DropdownMenu>
   )
