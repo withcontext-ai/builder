f62abd8fd95ad1d2cac055b23379282b06db0560
zdx <zdx@zdxMc.local>
2023-07-24 11:52:15 +0800

    fix: lastName is null

--- apps/web/src/components/auth-button.tsx
+++ apps/web/src/components/auth-button.tsx
@@ -8,7 +8,7 @@ export default async function AuthButton() {
 
   const { emailAddresses, firstName, lastName, imageUrl } =
     (await currentUser()) ?? {}
-  const name = `${firstName} ${lastName}`
+  const name = [firstName, lastName].join(' ')
   const email = emailAddresses?.[0].emailAddress
 
   return (
