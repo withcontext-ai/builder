6e649cf8001b46bb3a29c43f8668a1c145acb07f
zdx <zdx@zdxMc.local>
2023-07-24 16:14:41 +0800

    fix: dataset card docs

--- apps/web/src/components/dataset-card.tsx
+++ apps/web/src/components/dataset-card.tsx
@@ -14,20 +14,23 @@ import { PdfImage } from './upload/component'
 interface IProps {
   id: string
   title: string
+  config?: Record<string, any>
 }
 
-export default function DatasetCard({ id, title }: IProps) {
+export default function DatasetCard({ id, title, config }: IProps) {
   return (
     <Link href={`/dataset/${id}`}>
       <Card className="flex h-[148px] flex-col p-6 hover:shadow-md">
         <CardHeader className="h-full p-0">
           <CardTitle className="flex flex-1 gap-4 text-lg">
-            <PdfImage />
+            {config?.loaderType === 'pdf' && <PdfImage />}
+
             <div className="line-clamp-2 max-h-[52px] flex-1">{title}</div>
           </CardTitle>
           <CardFooter className="flex h-5 w-full gap-3 p-0">
             <div className="flex text-xs font-medium	text-slate-500">
-              <Files size={18} color="#64748B" />2 docs
+              <Files size={18} color="#64748B" />
+              {config?.files?.length} docs
             </div>
             <div className="flex text-xs font-medium	text-slate-500">
               <FileType2 size={18} color="#64748B" />
