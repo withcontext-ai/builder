499c88a5cbfed1a5e95d901195ae51c3aa434558
lizunlong <lizunlong@gmail.com>
2023-07-24 20:25:39 +0800

    fix: tiny change

--- apps/web/src/components/dataset-card.tsx
+++ apps/web/src/components/dataset-card.tsx
@@ -1,15 +1,11 @@
 import Link from 'next/link'
-import { FileOutput, Files, FileType2 } from 'lucide-react'
 
 import {
   Card,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
-
-import { PdfImage } from './upload/component'
 
 interface IProps {
   id: string
@@ -19,7 +15,7 @@ interface IProps {
 export default function DatasetCard({ id, title }: IProps) {
   return (
     <Link href={`/dataset/${id}`}>
-      <Card className="flex h-[148px] flex-col p-6 hover:shadow-md">
+      <Card className="h-[132px] hover:shadow-md">
         <CardHeader>
           <CardTitle className="line-clamp-3 text-lg">{title}</CardTitle>
         </CardHeader>
