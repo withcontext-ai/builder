904fafd1acafc4d5dbb06330505ea9d638364f4f
zdx <zdx@zdxMc.local>
2023-07-24 15:33:30 +0800

    fix: accept bug

--- apps/web/src/components/dataset-card.tsx
+++ apps/web/src/components/dataset-card.tsx
@@ -20,23 +20,8 @@ export default function DatasetCard({ id, title }: IProps) {
   return (
     <Link href={`/dataset/${id}`}>
       <Card className="flex h-[148px] flex-col p-6 hover:shadow-md">
-        <CardHeader className="h-full p-0">
-          <CardTitle className="flex flex-1 gap-4 text-lg">
-            <PdfImage />
-            <div className="line-clamp-2 max-h-[52px] flex-1">{title}</div>
-          </CardTitle>
-          <CardFooter className="flex h-5 w-full gap-3 p-0">
-            <div className="flex text-xs font-medium	text-slate-500">
-              <Files size={18} color="#64748B" />2 docs
-            </div>
-            <div className="flex text-xs font-medium	text-slate-500">
-              <FileType2 size={18} color="#64748B" />
-              2.8k words
-            </div>
-            <div className="flex text-xs font-medium	text-slate-500">
-              <FileOutput size={18} color="#64748B" />2 linked apps
-            </div>
-          </CardFooter>
+        <CardHeader className="h-[132px] hover:shadow-md">
+          <CardTitle className="line-clamp-3 text-lg">{title}</CardTitle>
         </CardHeader>
       </Card>
     </Link>
