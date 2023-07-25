f278fa4386ab3624ea03a3588dd731391fef3fdc
lizunlong <lizunlong@gmail.com>
2023-07-25 19:43:31 +0800

    feat: show workflow at app page

--- apps/web/src/app/app/[app_id]/workflow/page.tsx
+++ apps/web/src/app/app/[app_id]/workflow/page.tsx
@@ -1,3 +1,7 @@
+import { safeParse } from '@/lib/utils'
+import { getApp } from '@/db/apps/actions'
+
+import { WorkflowProvider } from './store'
 import TaskList from './task-list'
 
 interface IProps {
@@ -6,14 +10,25 @@ interface IProps {
 
 export default async function Page({ params }: IProps) {
   const { app_id } = params
+  const appDetail = await getApp(app_id)
+  const { published_workflow_tree_str, published_workflow_data_str } = appDetail
+  const workflowTree = safeParse(published_workflow_tree_str, [])
+  const workflowData = safeParse(published_workflow_data_str, [])
 
   return (
-    <div className="flex h-full flex-col">
-      <div className="flex h-12 shrink-0 items-center justify-between px-6">
-        <h1>Workflow</h1>
+    <WorkflowProvider
+      workflowTree={workflowTree}
+      workflowData={workflowData}
+      // selectedTaskId={workflowTree[0]?.id ?? null}
+      selectedTaskId={null}
+    >
+      <div className="flex h-full flex-col">
+        <div className="flex h-12 shrink-0 items-center justify-between px-6">
+          <h1>Workflow</h1>
+        </div>
+        <div className="h-px w-full shrink-0 bg-slate-100" />
+        <TaskList />
       </div>
-      <div className="h-px w-full shrink-0 bg-slate-100" />
-      <TaskList />
-    </div>
+    </WorkflowProvider>
   )
 }
