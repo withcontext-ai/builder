a96e31c6b3893704f19ab610aa1a7010e7ff60fe
lizunlong <lizunlong@gmail.com>
2023-07-27 14:48:47 +0800

    fix: dotenv

--- apps/web/drizzle.config.ts
+++ apps/web/drizzle.config.ts
@@ -1,13 +1,15 @@
 import * as dotenv from 'dotenv'
 import type { Config } from 'drizzle-kit'
 
-dotenv.config()
+dotenv.config({ path: '.env.local' })
+
+const connectionString = `${process.env.DATABASE_URL}?sslmode=require`
 
 export default {
   schema: './src/db/**/schema.ts',
   driver: 'pg',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL || '',
+    connectionString,
   },
   out: './drizzle',
 } satisfies Config
