a7f19bf4628b0d19b462ac8cdeb6ab9e80d3943c
lizunlong <lizunlong@gmail.com>
2023-07-28 18:50:37 +0800

    feat: users log

--- apps/web/src/db/users/actions.ts
+++ apps/web/src/db/users/actions.ts
@@ -3,15 +3,27 @@ import 'server-only'
 import { eq } from 'drizzle-orm'
 
 import { db } from '@/lib/drizzle'
+import { serverLog } from '@/lib/posthog'
 
 import { NewUser, UsersTable } from './schema'
 
 export async function addUser(user: NewUser) {
   try {
     const response = await db.insert(UsersTable).values(user).returning()
+    serverLog.capture({
+      distinctId: 'clerk_webhook',
+      event: 'success:add_user',
+      properties: {
+        user_id: response?.[0]?.short_id,
+        value: response?.[0],
+      },
+    })
     return { response }
   } catch (error: any) {
-    console.log('addUser error:', error)
+    serverLog.capture({
+      distinctId: 'clerk_webhook',
+      event: 'error:add_user',
+    })
     return {
       error: error.message,
     }
@@ -25,9 +37,24 @@ export async function editUser(id: string, newValue: Partial<NewUser>) {
       .set(newValue)
       .where(eq(UsersTable.short_id, id))
       .returning()
+    serverLog.capture({
+      distinctId: 'clerk_webhook',
+      event: 'success:edit_user',
+      properties: {
+        user_id: id,
+        value: newValue,
+      },
+    })
     return { response }
   } catch (error: any) {
-    console.log('editUser error:', error)
+    serverLog.capture({
+      distinctId: 'clerk_webhook',
+      event: 'error:edit_user',
+      properties: {
+        user_id: id,
+        value: newValue,
+      },
+    })
     return {
       error: error.message,
     }
