8bd0d89ca02dd42d066a30390aa9be0bf6c3ed82
lizunlong <lizunlong@gmail.com>
2023-08-05 21:35:32 +0800

    fix: taskToApiFormatter

--- apps/web/src/app/app/[app_id]/settings/workflow/utils.ts
+++ apps/web/src/app/app/[app_id]/settings/workflow/utils.ts
@@ -5,6 +5,10 @@ import { WorkflowItem } from './type'
 export function taskToApiFormatter(task: WorkflowItem) {
   const { key, type, subType } = task
   const chain = safeParse(task.formValueStr, {})
+  if (chain.data) {
+    chain.datasets = chain.data.datasets || []
+    delete chain.data
+  }
   return {
     key: `${type}-${key}`,
     chain_type: subType,
