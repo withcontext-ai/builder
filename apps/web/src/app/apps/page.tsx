f5da7fac3cbd8cab3493d448c64e516e83365fa6
lizunlong <lizunlong@gmail.com>
2023-08-15 15:41:35 +0800

    feat: page loading (#115)
    * feat: explore loading
    
    * feat: apps and datasets loading

--- apps/web/src/app/apps/page.tsx
+++ apps/web/src/app/apps/page.tsx
@@ -1,15 +1,14 @@
+import { Suspense } from 'react'
 import { PlusIcon } from 'lucide-react'
 
-import { getApps } from '@/db/apps/actions'
 import { Button } from '@/components/ui/button'
-import AppCard from '@/components/app-card'
 import CreateAppDialog from '@/components/create-app-dialog'
 import MineList from '@/components/mine-list'
 import RootWrapper from '@/components/root-wrapper'
 
-export default async function Page() {
-  const appList = await getApps()
+import CardList, { CardListFallback } from './card-list'
 
+export default function Page() {
   return (
     <RootWrapper pageTitle="My Space" nav={<MineList />}>
       <div className="flex flex-col">
@@ -31,17 +30,9 @@ export default async function Page() {
         </div>
         <div className="m-full hidden h-px shrink-0 bg-slate-200 lg:block" />
         <div className="p-6">
-          <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-            {appList.map(({ short_id, name, description, icon }) => (
-              <AppCard
-                key={short_id}
-                id={short_id}
-                name={name}
-                description={description}
-                icon={icon}
-              />
-            ))}
-          </ul>
+          <Suspense fallback={<CardListFallback />}>
+            <CardList />
+          </Suspense>
         </div>
       </div>
     </RootWrapper>
