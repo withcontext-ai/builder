95558f066d711a894bcc4d9191ae2573ce506d54
zdx <zdx@zdxMc.local>
2023-08-16 16:57:23 +0800

    fix: error change

--- apps/web/src/app/app/[app_id]/settings/workflow/prompt-mentions.tsx
+++ apps/web/src/app/app/[app_id]/settings/workflow/prompt-mentions.tsx
@@ -58,7 +58,7 @@ function PromptMentions<T extends FieldValues>({
               onChange={(e) => {
                 onChange(e.target.value)
                 // @ts-ignore
-                form.setValue(name, e?.target?.value?.replaceAll(' ', ''))
+                form.setValue(name, e?.target?.value)
               }}
               customSuggestionsContainer={(children) => (
                 <div className="w-[212px] rounded-md border bg-white p-2 text-sm font-medium shadow-sm">
