4924991ab8e40501142f157c5b3277fb5637cba2
zdx <zdx@zdxMc.local>
2023-08-17 17:59:23 +0800

    fix: remove unuse words

--- apps/web/src/__tests__/chat.test.tsx
+++ apps/web/src/__tests__/chat.test.tsx
@@ -1,12 +1,5 @@
-import {
-  fireEvent,
-  getByText,
-  render,
-  renderHook,
-  screen,
-} from '@testing-library/react'
-// import { enableFetchMocks } from 'jest-fetch-mock'
-import { beforeEach, expect, test } from 'vitest'
+import { fireEvent, render, renderHook } from '@testing-library/react'
+import { expect, test } from 'vitest'
 
 import '@testing-library/jest-dom'
 
@@ -14,62 +7,63 @@ import { useChat } from 'ai/react'
 
 import TestChat from '@/app/demo/test-chat/page'
 
-// enableFetchMocks()
-
 test('test chat show the default UI', () => {
-  const { queryByText, getByPlaceholderText } = render(<TestChat />)
+  const { queryByText, getByPlaceholderText, queryByRole } = render(
+    <TestChat />
+  )
 
   // chat header, session-name
-  expect(queryByText('test-chat-session')).toBeDisabled
+  expect(queryByText('test-chat-session')).toBeDefined()
 
   // chat list
 
   // chat input
-  expect(getByPlaceholderText('Type a message')).toBeInTheDocument
-  expect(queryByText('send')).toBeInTheDocument
+  expect(getByPlaceholderText('Type a message')).toBeInTheDocument()
+  expect(queryByText('send')).toBeDefined()
+  expect(queryByRole('button')).toBeDisabled()
 })
 
-// test('test chat when enter press to send msg', () => {
-//   // TODO: why multi textarea
-//   const { getAllByPlaceholderText, queryByText, getByRole } = render(
-//     <TestChat />
-//   )
-//   const textarea = getAllByPlaceholderText('Type a message')
-//   const input = 'hello, to test the textarea keypress'
+test('test chat when enter press to send msg', () => {
+  // TODO: why multi textarea
+  const { getAllByPlaceholderText, queryByText, getByRole } = render(
+    <TestChat />
+  )
+  const textarea = getAllByPlaceholderText('Type a message')
+  const input = 'hello, to test the textarea keypress'
 
-//   fireEvent.keyPress(textarea?.[0], {
-//     key: 'Enter',
-//     code: 13,
-//     charCode: 13,
-//     target: { value: input },
-//   })
+  fireEvent.keyPress(textarea?.[0], {
+    key: 'Enter',
+    code: 13,
+    charCode: 13,
+    target: { value: input },
+  })
 
-//   // add a new message
-//   expect(queryByText(input)).toBeDefined
+  // add a new message
+  expect(queryByText(input)).toBeDefined
 
-//   // show the message card  app_name: chat-app
-//   expect(queryByText('chat-app'))?.toBeInTheDocument
+  // show the message card  app_name: chat-app
+  expect(queryByText('chat-app'))?.toBeInTheDocument
 
-//   // loading status: waiting for response, can't keypress
-//   expect(getByRole('button')).toBeDisabled
+  // loading status: waiting for response, can't keypress
+  expect(getByRole('button')).toBeDisabled
 
-//   // show the stop generate button
-//   const stopButton = queryByText('Stop generating')
-//   expect(stopButton).not.toBeDisabled
-// })
+  // show the stop generate button
+  const stopButton = queryByText('Stop generating')
+  expect(stopButton).not.toBeDisabled
+})
 
-// test('test chat when click button to send msg', async () => {
-//   const { getByRole, queryByText } = render(<TestChat />)
-//   const button = getByRole('button')
+test('test chat when click button to send msg', async () => {
+  const { getByRole, queryByText } = render(<TestChat />)
+  const button = getByRole('button')
 
-//   fireEvent.click(button)
-//   // loading status: waiting for response, can't keypress
-//   expect(getByRole('button')).toBeDisabled
+  fireEvent.click(button)
+  // loading status: waiting for response, can't keypress
+  expect(getByRole('button')).toBeDisabled
 
-//   // show the stop generate button
-//   const stopButton = queryByText('Stop generating')
-//   expect(stopButton).not.toBeDisabled
-// })
+  // show the stop generate button
+  const stopButton = queryByText('Stop generating')
+  expect(stopButton).not.toBeDisabled
+})
 
 test('test useChat', async () => {
   const { result } = renderHook(() =>
@@ -83,6 +77,6 @@ test('test useChat', async () => {
     })
   )
 
-  expect(result?.current?.isLoading).toBeFalsy
-  expect(result?.current?.messages).not.toHaveLength
+  expect(result?.current?.isLoading).toBeFalsy()
+  expect(result?.current?.messages).toHaveLength(0)
 })
