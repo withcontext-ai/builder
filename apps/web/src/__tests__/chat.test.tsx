28f14eed1b479d031a9fa819224feba8445ad421
zdx <zdx@zdxMc.local>
2023-08-17 16:19:26 +0800

    fix: add jest-fetch-mock

--- apps/web/src/__tests__/chat.test.tsx
+++ apps/web/src/__tests__/chat.test.tsx
@@ -5,6 +5,7 @@ import {
   renderHook,
   screen,
 } from '@testing-library/react'
+import { enableFetchMocks } from 'jest-fetch-mock'
 import { beforeEach, expect, test } from 'vitest'
 
 import '@testing-library/jest-dom'
@@ -13,17 +14,19 @@ import { useChat } from 'ai/react'
 
 import TestChat from '@/app/demo/test-chat/page'
 
+enableFetchMocks()
+
 test('test chat show the default UI', () => {
   const { queryByText, getByPlaceholderText } = render(<TestChat />)
 
-  // // chat header, session-name
-  // expect(queryByText('test-chat-session')).toBeDisabled
+  // chat header, session-name
+  expect(queryByText('test-chat-session')).toBeDisabled
 
-  // // chat list
+  // chat list
 
-  // // chat input
-  // expect(getByPlaceholderText('Type a message')).toBeInTheDocument
-  // expect(queryByText('send')).toBeInTheDocument
+  // chat input
+  expect(getByPlaceholderText('Type a message')).toBeInTheDocument
+  expect(queryByText('send')).toBeInTheDocument
 })
 
 // test('test chat when enter press to send msg', () => {
