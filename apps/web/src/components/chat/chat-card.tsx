654df9b54c4657764dd8ea2620ed180857d44c21
lizunlong <lizunlong@gmail.com>
2023-08-23 14:31:54 +0800

    chore: add a log for debug

--- apps/web/src/components/chat/chat-card.tsx
+++ apps/web/src/components/chat/chat-card.tsx
@@ -120,6 +120,7 @@ const ChatCard = (props: IProps) => {
   const showError = isEnd && error && !isUser
 
   const { user } = useUser()
+  console.log('user?.imageUrl:', user?.imageUrl)
 
   const color = getAvatarBgColor(appId || '')
   const username = [user?.firstName || '', user?.lastName || '']
