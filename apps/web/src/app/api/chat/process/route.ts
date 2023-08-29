7190bf6533f0b30ecb40c74aa89c44906665f73e
zdx <zdx@zdxMc.local>
2023-08-29 11:43:52 +0800

    fix: tiny change

--- apps/web/src/app/api/chat/process/route.ts
+++ apps/web/src/app/api/chat/process/route.ts
@@ -1,13 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server'
 import axios from 'axios'
 
-// Get an app
+// Get chat process
 export async function POST(req: NextRequest) {
   const params = await req.json()
   const { api_session_id } = params
-  // to get chat-process
   let { data } = await axios.post(
-    `${process.env.AI_SERVICE_API_BASE_URL}/v1/chat-process/${api_session_id}`
+    `${process.env.AI_SERVICE_API_BASE_URL}//${api_session_id}`
   )
   if (data.status !== 200) {
     return
