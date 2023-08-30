3a07defeee05d8982c48499c3e43609df9478946
lizunlong <lizunlong@gmail.com>
2023-08-30 17:44:16 +0800

    feat: get tasks

--- apps/web/src/app/app/[app_id]/session/[session_id]/page.tsx
+++ apps/web/src/app/app/[app_id]/session/[session_id]/page.tsx
@@ -1,4 +1,5 @@
 import { safeParse } from '@/lib/utils'
+import { App } from '@/db/apps/schema'
 import { getSession } from '@/db/sessions/actions'
 import Chat from '@/components/chat/page'
 
@@ -7,6 +8,21 @@ import AppNotFound from './app-not-found'
 
 // export const runtime = 'edge'
 
+function getTasks(app: App) {
+  const tree = safeParse(app.published_workflow_tree_str, [])
+  const data = safeParse(app.published_workflow_data_str, [])
+  const tasks = tree.map((t: any) => {
+    const d = data.find((d: any) => d.id === t.id)
+    return {
+      id: d.id,
+      type: d.type,
+      subType: d.subType,
+      key: d.key,
+    }
+  })
+  return tasks
+}
+
 interface IProps {
   params: { app_id: string; session_id: string }
 }
@@ -14,6 +30,9 @@ interface IProps {
 export default async function SessionPage({ params }: IProps) {
   const { app_id, session_id } = params
   const { session, app, user } = await getSession(session_id, app_id)
+
+  const tasks = app ? getTasks(app) : []
+  console.log('tasks:', tasks)
 
   return (
     <>
