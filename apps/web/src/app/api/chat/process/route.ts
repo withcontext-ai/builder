24df55a1ac7b6c39795ca9fac18241b05edf3e76
lizunlong <lizunlong@gmail.com>
2023-08-30 14:17:41 +0800

    fix: process route

--- apps/web/src/app/api/chat/process/route.ts
+++ apps/web/src/app/api/chat/process/route.ts
@@ -2,14 +2,20 @@ import { NextRequest, NextResponse } from 'next/server'
 import axios from 'axios'
 
 // Get chat process
-export async function POST(req: NextRequest) {
-  const params = await req.json()
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { api_session_id: string } }
+) {
   const { api_session_id } = params
-  let { data } = await axios.post(
-    `${process.env.AI_SERVICE_API_BASE_URL}//${api_session_id}`
+
+  let { data: res } = await axios.get(
+    `${process.env.AI_SERVICE_API_BASE_URL}/v1/chat/session/${api_session_id}/process`
   )
-  if (data.status !== 200) {
-    return
+  if (res.status !== 200) {
+    throw new Error(`API service error: ${res.message}`)
   }
-  return NextResponse.json({ success: true, data })
+
+  const data = res?.data
+
+  return NextResponse.json({ success: true, data: { api_session_id } })
 }
