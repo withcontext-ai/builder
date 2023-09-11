6da7b3f62b9f8a2530defff73ae5d24e30bff0fb
lizunlong <lizunlong@gmail.com>
2023-09-11 20:23:52 +0800

    chore: clean code

--- apps/web/src/lib/drizzle-pool.ts
+++ apps/web/src/lib/drizzle-pool.ts
@@ -1,9 +1,9 @@
-import { Pool as PoolServerless } from '@neondatabase/serverless'
-import { drizzle as drizzleServerless } from 'drizzle-orm/neon-serverless'
+import { Pool } from '@neondatabase/serverless'
+import { drizzle } from 'drizzle-orm/neon-serverless'
 
-const pool = new PoolServerless({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   connectionTimeoutMillis: 10 * 1000, // wait 10s before timing out when connecting a new client
 })
 
-export const db = drizzleServerless(pool as PoolServerless)
+export const db = drizzle(pool)
