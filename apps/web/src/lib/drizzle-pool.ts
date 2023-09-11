2a7584010175de1f1dba4de3a3d59b099779d587
lizunlong <lizunlong@gmail.com>
2023-09-11 21:11:57 +0800

    fix: not includes raw data in messages action (#163)

--- apps/web/src/lib/drizzle-pool.ts
+++ apps/web/src/lib/drizzle-pool.ts
@@ -1,9 +1,15 @@
 import { Pool } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-serverless'
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  connectionTimeoutMillis: 10 * 1000, // wait 10s before timing out when connecting a new client
-})
+export function createPool() {
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    connectionTimeoutMillis: 10 * 1000, // wait 10s before timing out when connecting a new client
+  })
+  const db = drizzle(pool)
 
-export const db = drizzle(pool)
+  return {
+    pool,
+    db,
+  }
+}
