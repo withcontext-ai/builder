d6300895a86a392596f2bd4617f0b79497739a6b
lizunlong <lizunlong@gmail.com>
2023-09-19 13:44:07 +0800

    fix: layout

--- apps/web/src/app/app/[app_id]/share/slack.tsx
+++ apps/web/src/app/app/[app_id]/share/slack.tsx
@@ -70,15 +70,17 @@ const SlackDialog = NiceModal.create(({}: IProps) => {
           </DialogDescription>
         </DialogHeader>
 
-        <a href={authorizeUrl} target="_blank">
-          <img
-            alt="Add to Slack"
-            height="40"
-            width="139"
-            src="https://platform.slack-edge.com/img/add_to_slack.png"
-            srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-          />
-        </a>
+        <div className="flex">
+          <a href={authorizeUrl} target="_blank">
+            <img
+              alt="Add to Slack"
+              height="40"
+              width="139"
+              src="https://platform.slack-edge.com/img/add_to_slack.png"
+              srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+            />
+          </a>
+        </div>
 
         <div className="space-y-2">
           {teamList.map(
