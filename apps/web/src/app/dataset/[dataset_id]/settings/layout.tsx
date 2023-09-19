56256ab5b7ee5aaac7b8af116e91fc27c43cee91
zdx <zdx@zdxMc.local>
2023-09-19 19:27:18 +0800

    fix: segment scroll error

--- apps/web/src/app/dataset/[dataset_id]/settings/layout.tsx
+++ apps/web/src/app/dataset/[dataset_id]/settings/layout.tsx
@@ -13,11 +13,11 @@ export default async function SettingsLayout({ children, params }: IProps) {
   const { dataset_id } = params
   const datasetDetail = await getDataset(dataset_id)
   return (
-    <div className="flex h-full w-full bg-white">
+    <div className="hidden h-full w-full lg:flex">
       <div className="hidden w-[276px] shrink-0 border-r border-slate-200 bg-slate-50 lg:block">
         <SlideBar datasetId={dataset_id} name={datasetDetail?.name} />
       </div>
-      <div className="flex-1">{children}</div>
+      <div className="h-full w-full">{children}</div>
     </div>
   )
 }
