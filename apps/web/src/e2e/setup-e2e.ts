f5bc5b629c1540809cdda810d7dcebce719b3b0b
Xiaofei Liao <felixliaochina@gmail.com>
2023-09-25 16:33:00 +0800

    test: env

--- apps/web/src/e2e/setup-e2e.ts
+++ apps/web/src/e2e/setup-e2e.ts
@@ -2,12 +2,16 @@ import path from 'path'
 import { expect, test } from '@playwright/test'
 import dotenv from 'dotenv'
 
-// local
-if (!process.env.CI) {
-  dotenv.config({
-    path: path.resolve(__dirname, '..', '..', '.env.local'),
-  })
-}
+console.log('process.env', process.env)
+
+dotenv.config({
+  path: path.resolve(
+    __dirname,
+    '..',
+    '..',
+    process.env.CI ? '.env' : '.env.local'
+  ),
+})
 
 const E2E_USERNAME = process.env.E2E_USERNAME!
 const E2E_PASSWORD = process.env.E2E_PASSWORD!
