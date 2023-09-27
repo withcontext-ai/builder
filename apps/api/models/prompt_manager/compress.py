168822505eab6117e687d0d8d6a438a0b22ef0de
zhangzhihao <1900017707@pku.edu.cn>
2023-09-27 15:06:22 +0800

    fix: human input loss

---
+++ apps/api/models/prompt_manager/compress.py
@@ -0,0 +1,63 @@
+import tiktoken
+from langchain.chains.summarize import load_summarize_chain
+from langchain.llms import OpenAI
+from langchain.schema import Document
+from langchain.memory import ConversationSummaryMemory
+
+
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
+    """Return the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model in {
+        "gpt-3.5-turbo-0613",
+        "gpt-3.5-turbo-16k-0613",
+        "gpt-4-0314",
+        "gpt-4-32k-0314",
+        "gpt-4-0613",
+        "gpt-4-32k-0613",
+    }:
+        tokens_per_message = 3
+        tokens_per_name = 1
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = (
+            4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        )
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif "gpt-3.5-turbo" in model:
+        print(
+            "Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613."
+        )
+        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613")
+    elif "gpt-4" in model:
+        print(
+            "Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613."
+        )
+        return num_tokens_from_messages(messages, model="gpt-4-0613")
+    else:
+        raise NotImplementedError(
+            f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
+        )
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    return num_tokens
+
+
+async def summrize_content(content, chain_type):
+    summrize_chain = load_summarize_chain(llm=OpenAI(), chain_type=chain_type)
+    return await summrize_chain.acombine_docs([Document(content=content)])
+
+
+async def sumrize_message(messages, chain_type):
+    summrize_chain = load_summarize_chain(llm=OpenAI(), chain_type=chain_type)
+    docs = [Document(content=message) for message in messages]
+    return await summrize_chain.acombine_docs(docs)
