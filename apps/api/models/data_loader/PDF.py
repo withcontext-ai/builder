554134a55275e755040c3f7db57e86534093d979
zhangzhihao <1900017707@pku.edu.cn>
2023-09-27 16:20:46 +0800

    fix: change charcters logic

--- apps/api/models/data_loader/PDF.py
+++ apps/api/models/data_loader/PDF.py
@@ -51,7 +51,7 @@ class PDFLoader:
                     )
                     pdf_content = storage_client.load(document.url)
                     text = PDFLoader.extract_text_from_pdf(pdf_content)
-                    document.content_size = sys.getsizeof(text)
+                    document.content_size = len(text)
                     pages = text.split("\f")
                     for page in pages:
                         _doc.append(
