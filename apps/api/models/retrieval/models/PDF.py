b5a54982159211945a1c97f8c9e9a9d9a01e7571
linxiaowei <linxiaowei@zhihu.com>
2023-10-09 01:59:35 +1100

    fix

--- apps/api/models/retrieval/models/PDF.py
+++ apps/api/models/retrieval/models/PDF.py
@@ -48,16 +48,15 @@ class PatchedSelfQueryRetriever(SelfQueryRetriever):
         elif self.search_type == "similarity_score_threshold":
             docs_and_similarities = (
                 await self.vectorstore.asimilarity_search_with_relevance_scores(
-                    query, k=10000, **self.search_kwargs
+                    query, 10000, **self.search_kwargs
                 )
             )
             docs = [doc for doc, _ in docs_and_similarities]
             docs = [doc for doc in docs if query.lower() in doc.page_content.lower()]
         elif self.search_type == "mmr":
             docs = await self.vectorstore.amax_marginal_relevance_search(
-                query, k=10000,fetch_k=10000,lambda_mult=0, **self.search_kwargs
+                query,  **self.search_kwargs
             )
-            docs = [doc for doc in docs if query.lower() in doc.page_content.lower()]
         else:
             raise ValueError(f"search_type of {self.search_type} not allowed.")
 
