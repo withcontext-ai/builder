14c051a89019ace0ec22a7cdfb0bac4db2210c5a
zdx <zdx@zdxMc.local>
2023-10-09 19:49:17 +0800

    fix: ue

--- apps/web/src/app/api/datasets/segment/route.ts
+++ apps/web/src/app/api/datasets/segment/route.ts
@@ -13,11 +13,9 @@ export async function GET(req: NextRequest) {
   const uid = query.get('uid') || ''
   const limit = parseInt(query.get('pageSize') || '')
   const page = parseInt(query.get('pageIndex') || '')
-  const pageSize = page ? page - 1 : 0
   const search = query.get('search') || ''
-  const offset = pageSize * limit
-  const res = await getSegments(dataset_id, uid, search, offset, limit)
-  const data = res?.data
+  const offset = page * limit
+  const data = await getSegments(dataset_id, uid, search, offset, limit)
   const segments =
     data?.segments?.length === 0
       ? [{ content: '', segment_id: '01' }]
