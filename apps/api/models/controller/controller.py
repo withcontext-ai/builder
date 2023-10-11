b7338a991d592421d384ca99829308b33c3e60cf
linxiaowei <linxiaowei@zhihu.com>
2023-10-11 18:35:42 +1100

    fix

--- apps/api/models/controller/controller.py
+++ apps/api/models/controller/controller.py
@@ -367,8 +367,15 @@ class DatasetManager(BaseManager):
                             seg_ids = [f"{dataset_id}-{matching_url}-{i}" for i in range(doc.hundreaith_id[-1], doc.page_size)]
                             vectors = Retriever.fetch_vectors(ids=seg_ids)
                             if len(vectors) >= 100:
-                                next_hundreaith = doc.hundredth_ids[-1] + 100
-                                doc.hundredth_ids.append(next_hundreaith)
+                                next_hundredth = doc.hundredth_ids[-1] + 100
+                                doc.hundredth_ids.append(next_hundredth)
+                    else:
+                        if doc.page_size >= 99:
+                            seg_ids = [f"{dataset_id}-{matching_url}-{i}" for i in range(0, doc.page_size)]
+                            vectors = Retriever.fetch_vectors(ids=seg_ids)
+                            if len(vectors) >= 100:
+                                next_hundredth = 99
+                                doc.hundredth_ids.append(next_hundredth)
                 else:
                     # Handle edit
                     segment_length = len(
