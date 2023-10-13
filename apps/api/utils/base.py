26f7a7382ddd8e85eb8040334ece65fd37cc28a9
zhangzhihao <1900017707@pku.edu.cn>
2023-10-13 11:40:23 +0800

    fix: filt bytes content

--- apps/api/utils/base.py
+++ apps/api/utils/base.py
@@ -1,7 +1,12 @@
+from loguru import logger
+
+
 def to_string(content):
     if isinstance(content, str):
         return content
     elif isinstance(content, bytes):
         return content.decode()
+
     else:
-        raise ValueError(f"type {type(content)} is not supported")
+        logger.warning(f"type {type(content)} is not supported.")
+        return content
