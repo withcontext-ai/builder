733efaba1636fbdfac9bed99ccb011364d1fe303
lizunlong <lizunlong@gmail.com>
2023-10-12 22:40:14 -0500

    feat: new apps at Explore page & sort based on sequence of ids (#201)
    * feat: sort by sequence of ids
    
    * feat: new explore apps

--- apps/web/src/app/explore/utils.ts
+++ apps/web/src/app/explore/utils.ts
@@ -1,88 +1,82 @@
-// Language Assistant
-const LA = [
+// Q&A of your Data
+const RETRIEVAL = [
+  '8B9OI3ddW0YR', // A knowledge base Q&A chatbot for a certain company
+  'knfBakzXl0E0', // 24-Hour After-Sales Customer Service
+  'xBvYMaEhicmp', // Reading Books - The Great Gatsby
+  'GwSCco2hFnZR', // Speed-reading insurance terms and conditions
+  'CMTiWOdsFCbp', // AI Restaurant Assistant
+]
+
+// Mock Interview
+const INTERVIEW = [
+  'EtxOfgdIrTBH', // Software Engineer-Interviewer
+  'g42PpZERXi0b', // Sales Manager Position-Interviewer
+  'EkD1cf4RnZfB', // Administrative position - interviewer
+  'Vjycf0aSuiEZ', // Registered Nurse, Internal Medicine/Surgery Unit
+  'UX9h89gIoAm5', // Medical Assistant
+  '1y4kuKxPrAkb', // Receptionist
+  'gztL9qrgFqp5', // Financial Services Audit
+  'BxIqm5FFq4Pk', // Web Developer
+  'ogrtYdowhY9R', // Customized Interviews
+  'WDLourw816W1', // Resume Rewrite
+  'tR9bV4fiCSVB', // Writing Cover Letters
+  'n1WRn2l3exDk', // Interview Question Generator
+]
+
+// Intelligent Forms
+const FORM = [
+  '7G74vr6Yk8j0', // New Member Registration on a Certain Platform
+]
+
+// Role Play
+const CHARACTER = [
+  'wxIBFg1PjSMl', // Elon Musk
+  'SWWCi3VYv5Qu', // Harry Potter
+  'J7Mtwdm74SxL', // Spider-Man
+  'ASSxL7CZv14s', // Bart Simpson
+]
+
+// Useful Assistant
+const ASSISTANT = [
   '884PItmQHsl4', // Translate English text into French
   'cYnwDNpUcR2o', // Chinese Translator
   'G0kCCxQ8EoTq', // Essay Improver
   'HXo4cna4e5xj', // As an English language teacher
-]
-
-// Famous Character
-const FC = [
-  'wxIBFg1PjSMl', // Play Elon Musk
-  'J7Mtwdm74SxL', // Play Spider-Man
-  'SWWCi3VYv5Qu', // Play Harry potter
-  '0O2nJnSpQURk', // Sun Wukong
-]
-
-// Writing
-const WR = [
   'fkuqiMciaE1t', // A title generator for written pieces
   't2lOpfSmCnB0', // Youtube Title(SEO optimized)
   '2a5wLkRBF72N', // Perfect Article
-]
-
-// Useful Assistant
-const UA = [
   'BE5sDxITFdze', // Code Error Fixer
   'kkWDKtFqkELc', // Video Script Outline
   'cEHybwkO6lDR', // Text Similarity Comparison
   '220Pf5KV5oXx', // Prompt Improvement
   'ax4kQ4S3eblG', // Master of Color Matching
-  'JBeHx3lqQxVI', // Little Red Book Hot Item Copy Generator
-]
-
-// Professional
-const PR = [
-  '3LMAJyVVSAgQ', // Virtual Doctor
   'wr9Xf9GAhnx6', // Product Manager
-  'eV7uEzpMRzw1', // legal advisor
-  'iBidZhqJSsBX', // Roleplay - Interviewer
-]
-
-// Search Document
-const SD = [
-  'LC2yEFdovW4B', // American history
-]
-
-// Interviewer
-const IN = [
-  'EtxOfgdIrTBH', // Software Engineer
-  'g42PpZERXi0b', // Sales Position
-  'BLUsUDwk2Yd4', // Investment Banking Intern
-  'EkD1cf4RnZfB', // Administrative position
+  'eV7uEzpMRzw1', // Legal advisor
 ]
 
 // All Categories
-const ALL = [...LA, ...FC, ...WR, ...UA, ...PR, ...SD, ...IN]
+const ALL = [...RETRIEVAL, ...INTERVIEW, ...FORM, ...CHARACTER, ...ASSISTANT]
 
 const CATEGORY_DATA = {
-  LA: {
-    title: 'Language Assistant',
-    ids: LA,
+  RETRIEVAL: {
+    title: 'Q&A of your Data',
+    ids: RETRIEVAL,
   },
-  FC: {
-    title: 'Famous Character',
-    ids: FC,
+  INTERVIEW: {
+    title: 'Mock Interview',
+    ids: INTERVIEW,
   },
-  WR: {
-    title: 'Writing',
-    ids: WR,
+  FORM: {
+    title: 'Intelligent Forms',
+    ids: FORM,
   },
-  UA: {
+  CHARACTER: {
+    title: 'Role Play',
+    ids: CHARACTER,
+  },
+  ASSISTANT: {
     title: 'Useful Assistant',
-    ids: UA,
-  },
-  PR: {
-    title: 'Professional',
-    ids: PR,
-  },
-  SD: {
-    title: 'Search Document',
-    ids: SD,
-  },
-  IN: {
-    title: 'Interviewer',
-    ids: IN,
+    ids: ASSISTANT,
   },
   ALL: {
     title: 'All Categories',
