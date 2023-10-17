d656687e079f2eda1b9bc6f400f90a4a2b5c94b4
Xiaofei Liao <felixliaochina@gmail.com>
2023-10-18 07:35:59 +0800

    refactor: loading

--- apps/web/src/app/app/[app_id]/layout.tsx
+++ apps/web/src/app/app/[app_id]/layout.tsx
@@ -1,4 +1,12 @@
+import { Suspense } from 'react'
+
+import { getApp } from '@/db/apps/actions'
+import { getSessions } from '@/db/sessions/actions'
+import { Skeleton } from '@/components/ui/skeleton'
 import AppLayout from '@/components/layouts/app-layout'
+import ResponsiveLayout from '@/components/layouts/responsive-layout'
+
+import Sidebar from './sidebar'
 
 // TODO: remove axios
 // export const runtime = 'edge'
@@ -8,8 +16,13 @@ interface IProps {
   params: { app_id: string; session_id: string }
 }
 
-export default function Layout({ children, params }: IProps) {
+export default async function Layout({ children, params }: IProps) {
   const { app_id } = params
-
-  return <AppLayout>{children}</AppLayout>
+  return (
+    <AppLayout>
+      <ResponsiveLayout sidebar={<Sidebar appId={app_id} />}>
+        {children}
+      </ResponsiveLayout>
+    </AppLayout>
+  )
 }
--- apps/web/src/app/app/[app_id]/sidebar.tsx
+++ apps/web/src/app/app/[app_id]/sidebar.tsx
@@ -1,6 +1,7 @@
-import { auth } from '@/lib/auth'
-import { getApp } from '@/db/apps/actions'
+import { Suspense } from 'react'
+
 import { getSessions } from '@/db/sessions/actions'
+import { Skeleton } from '@/components/ui/skeleton'
 import AuthButton from '@/components/auth-button'
 import SessionList from '@/components/session-list'
 import SessionListHeader from '@/components/session-list-header'
@@ -9,28 +10,26 @@ import Header from './sidebar-header'
 import Menu from './sidebar-menu'
 
 export default async function Sidebar({ appId }: { appId: string }) {
-  const { userId } = auth()
-  const appDetail = await getApp(appId)
-  const sessionList = await getSessions(appId)
-
-  const isOwner = userId === appDetail.created_by
-
   return (
     <div className="flex h-full overflow-hidden">
       <div className="flex w-60 shrink-0 grow flex-col bg-gray-50">
         <div className="flex-1 overflow-y-auto scrollbar-none">
-          <Header
-            appId={appId}
-            name={appDetail.name}
-            desc={appDetail.description}
-            icon={appDetail.icon}
-            isOwner={isOwner}
-          />
+          <Suspense fallback={<Skeleton className="h-32 w-full" />}>
+            <Header appId={appId} />
+          </Suspense>
           <div className="m-full h-px bg-slate-200" />
           <Menu />
           <div className="m-full h-px bg-slate-200" />
           <SessionListHeader appId={appId} />
-          <SessionList appId={appId} sessionList={sessionList} />
+          <Suspense
+            fallback={
+              <div className="space-y-1 p-2">
+                <Skeleton className="h-8 w-full bg-slate-200 p-2" />
+              </div>
+            }
+          >
+            <SessionList appId={appId} />
+          </Suspense>
         </div>
         <AuthButton />
       </div>
