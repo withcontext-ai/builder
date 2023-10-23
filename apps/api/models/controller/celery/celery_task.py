e6558a657937b513281b61626e4aa73fcab9e115
linxiaowei <linxiaowei@zhihu.com>
2023-10-23 11:53:50 +1100

    fix: celery

--- apps/api/models/controller/celery/celery_task.py
+++ apps/api/models/controller/celery/celery_task.py
@@ -25,19 +25,25 @@ password = UPSTASH_REDIS_REST_TOKEN
 app.conf.broker_url = f"rediss://:{password}@{broker_host}:{broker_port}/{broker_db}?ssl_cert_reqs=CERT_REQUIRED"
 app.conf.result_backend = f"rediss://:{password}@{broker_host}:{broker_port}/{results_db}?ssl_cert_reqs=CERT_REQUIRED"
 
-def retry_on_exception(task_func):
+def retry_on_exception(task_func, max_retries=3):
     @wraps(task_func)
     def wrapper(task_instance, *args, **kwargs):
-        try:
-            return task_func(task_instance, *args, **kwargs)
-        except Exception as e:
-            logger.error(f"Error in task {task_func.__name__}: {e}\n{traceback.format_exc()}")
+        retries = 0
+        while retries <= max_retries:
             try:
-                # Use task_instance.retry to retry the task.
-                task_instance.retry(countdown=60)
-            except MaxRetriesExceededError:
-                logger.error(f"Max retries exceeded for task {task_func.__name__}")
-
+                return task_func(task_instance, *args, **kwargs)
+            except Exception as e:
+                retries += 1
+                logger.error(f"Error in task {task_func.__name__} [Attempt {retries}/{max_retries}]: {e}\n{traceback.format_exc()}")
+                if retries <= max_retries:
+                    try:
+                        # Use task_instance.retry to retry the task.
+                        task_instance.retry(countdown=60)
+                    except MaxRetriesExceededError:  # Make sure this exception is defined or imported.
+                        logger.error(f"Max retries exceeded for task {task_func.__name__}")
+                        break
+                else:
+                    break
     return wrapper
 
 @app.task(bind=True)
