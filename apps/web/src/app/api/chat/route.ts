e1f007b2ccedcaa9c4bfef0f2f31e6346ca3c1dd
lizunlong <lizunlong@gmail.com>
2023-11-01 14:58:54 +0800

    chore: remove logs

--- apps/web/src/app/api/chat/route.ts
+++ apps/web/src/app/api/chat/route.ts
@@ -155,9 +155,8 @@ export async function POST(req: NextRequest, res: NextResponse) {
       id: messageId,
     },
   })
-  console.log('/api/chat')
+
   req.signal.onabort = () => {
-    console.log('/api/chat, onabort')
     stream.cancel()
   }
 
