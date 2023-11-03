b1b3147cf56138585bffd8e486b6bfb3a257be4d
dailly <daillyf@163.com>
2023-11-03 18:41:09 +0800

    fix: code style

--- apps/web/src/components/chat/markdown/markdown.tsx
+++ apps/web/src/components/chat/markdown/markdown.tsx
@@ -5,7 +5,7 @@ import remarkMath from 'remark-math'
 
 import { cn } from '@/lib/utils'
 
-import { CodeBlock, programmingLanguages } from './code-block'
+import { CodeBlock, CodeType } from './code-block'
 import { MarkdownProps } from './type'
 
 export const Markdown = (props: MarkdownProps) => {
@@ -27,12 +27,21 @@ export const Markdown = (props: MarkdownProps) => {
             <p className="prose-sm mb-2 font-normal last:mb-0">{children}</p>
           )
         },
+        pre({ children }) {
+          return (
+            <pre className={cn('p-1', isUser ? 'bg-primary' : 'bg-gray-100')}>
+              {children}
+            </pre>
+          )
+        },
         li({ children }) {
           return <li className="prose-sm">{children}</li>
         },
         code({ node, className, children, ...props }) {
           const match = /language-(\w+)/.exec(className || '')
-          const isCode = programmingLanguages?.[match?.[1] || '']
+          const isCode = CodeType?.filter(
+            (item) => item?.includes(match?.[1] || '')
+          )?.length
           if (isCode && match) {
             return (
               <CodeBlock
@@ -40,6 +49,16 @@ export const Markdown = (props: MarkdownProps) => {
                 value={String(children).replace(/\n$/, '')}
                 {...props}
               />
+            )
+          }
+          if (isCode) {
+            return (
+              <code
+                {...props}
+                className={cn(className, isUser ? 'text-white' : 'text-black')}
+              >
+                {children}
+              </code>
             )
           }
           return (
