7d9952674c6629df8df68c1affffe0081814ef10
zhangzhihao <1900017707@pku.edu.cn>
2023-11-07 17:45:11 +0800

    feat: swit ch llm model to 11-06 version

--- apps/api/test.py
+++
@@ -1,7 +0,0 @@
-import openai
-
-response = openai.completions.create(
-    model="text-davinci-003",
-    prompt="Translate the following English text to French: 'Hello, how are you?'",
-    response_format={"type": "json_object"},
-)
