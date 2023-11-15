05bb4a4efe3f9e2ebf2b1f0951e672200957abe6
lizunlong <lizunlong@gmail.com>
2023-11-15 17:48:22 +0800

    test: e2e timeout (#267)

--- apps/web/src/e2e/chat.spec.ts
+++ apps/web/src/e2e/chat.spec.ts
@@ -1,8 +1,8 @@
 import { expect, test } from '@playwright/test'
 
+const timeout = 10_000
+
 test('test', async ({ page }) => {
-  // eslint-disable-next-line turbo/no-undeclared-env-vars
-  console.log(process.env.BASE_URL)
   await page.goto('/explore')
   await page.getByRole('link', { name: 'E', exact: true }).click()
   const input = page.getByPlaceholder('Type a message')
@@ -14,24 +14,29 @@ test('test', async ({ page }) => {
   const lastMessage = messages[messages.length - 1]
   const secondLastMessage = messages[messages.length - 2]
   expect(await secondLastMessage.textContent()).toContain('hi')
-  expect(lastMessage.getByTestId('chat-card-loading')).toBeInViewport()
-  await expect(lastMessage.getByTestId('chat-card-loading')).not.toBeVisible({
-    timeout: 10000,
+  await expect(lastMessage.getByTestId('chat-card-loading')).toBeVisible({
+    timeout,
   })
+  await expect(lastMessage.getByTestId('chat-card-loading')).not.toBeVisible({
+    timeout,
+  })
+
   await page.getByRole('button', { name: 'Regenerate response' }).click()
   const loading = page
     .getByTestId('chat-card-content')
     .last()
     .getByTestId('chat-card-loading')
   expect(loading).toBeInViewport()
-  await expect(loading).not.toBeVisible()
+  await expect(loading).not.toBeVisible({ timeout })
+
   await page.getByPlaceholder('Type a message').click()
   await page.getByPlaceholder('Type a message').fill('what')
   await page.getByRole('button', { name: 'Send', disabled: false }).click()
-
   const messages2 = await page.getByTestId('chat-card-content').all()
   const lastMessage2 = messages2[messages2.length - 1]
-  expect(lastMessage2.getByTestId('chat-card-loading')).toBeInViewport()
+  await expect(lastMessage2.getByTestId('chat-card-loading')).toBeVisible({
+    timeout,
+  })
   await page.getByRole('button', { name: 'Stop generating' }).click()
-  expect(lastMessage2).not.toBeVisible()
+  await expect(lastMessage2).not.toBeVisible({ timeout })
 })
