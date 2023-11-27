d8587c02060a941a47e4c19fca4147ea7dda39e8
dailly <daillyf@163.com>
2023-11-27 16:16:35 +0800

    Revert "refactor: dataset loading state"
    This reverts commit 2499bf2c6fc1ffbb2785338270708e0601c378d5.

--- apps/web/src/app/dataset/[dataset_id]/settings/basics/loading.tsx
+++
@@ -1,3 +0,0 @@
-import Loading from '@/components/page-loading'
-
-export default Loading
--- apps/web/src/app/dataset/[dataset_id]/settings/layout.tsx
+++ apps/web/src/app/dataset/[dataset_id]/settings/layout.tsx
@@ -1,8 +1,4 @@
-import { Suspense } from 'react'
-
 import { getDataset } from '@/db/datasets/actions'
-import { Skeleton } from '@/components/ui/skeleton'
-import ManageLayout from '@/components/layouts/manage-layout'
 
 import Sidebar from './sidebar'
 
@@ -13,37 +9,15 @@ interface IProps {
   params: { dataset_id: string }
 }
 
-const Loading = (
-  <div>
-    <div className="flex items-center space-x-2 px-4 py-3">
-      <Skeleton className="h-8 w-1/2" />
-    </div>
-    <div className="mt-4 space-y-2 p-2">
-      <div className="px-2">
-        <Skeleton className="h-5 w-1/2" />
-      </div>
-      <Skeleton className="h-[84px] w-full" />
-      <Skeleton className="h-[84px] w-full" />
-      <div className="mb-2 shrink-0 px-3">
-        <div className="h-px bg-slate-200" />
-      </div>
-      <Skeleton className="h-9 w-full" />
-    </div>
-  </div>
-)
-
 export default async function SettingsLayout({ children, params }: IProps) {
   const { dataset_id } = params
   const datasetDetail = await getDataset(dataset_id)
   return (
-    <ManageLayout
-      sidebar={
-        <Suspense fallback={Loading}>
-          <Sidebar datasetId={dataset_id} name={datasetDetail?.name} />
-        </Suspense>
-      }
-    >
-      {children}
-    </ManageLayout>
+    <div className="h-full  w-full overflow-hidden bg-white lg:fixed lg:flex">
+      <div className="hidden w-[276px] shrink-0 border-r border-slate-200 bg-slate-50 lg:block">
+        <Sidebar datasetId={dataset_id} name={datasetDetail?.name} />
+      </div>
+      <div className="h-full w-full">{children}</div>
+    </div>
   )
 }
